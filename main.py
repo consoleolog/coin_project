30f4eb4cdf6ee412a7aa881a7a67a14bb4358808
consolelog <leopportunity5@gmail.com>
2024-07-17 16:16:38 +0900

    [1] 로그 설정 변경 반영

--- main.py
+++ main.py
@@ -1,47 +1,31 @@
-import pyupbit
 import time
-import os
-import pandas as pd
-from dotenv import load_dotenv
+import config
 from upbit_module import *
+import normal_log
 
-load_dotenv()
-
-access_key = os.environ['ACCESS_KEY']
-secret_key = os.environ['SECRET_KEY']
+access_key = config.UPBIT_ACCESS_KEY
+secret_key = config.UPBIT_SECRET_KEY
 
 ticker = "KRW-BTC"
 
-current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
 while True:
-    price = pyupbit.get_ohlcv(ticker, interval='day')
-    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=360)
-    df['close'] = df['close'].astype(int)
-    df['ma20'] = df['close'].astype(int).rolling(20).mean()
-    df['ma60'] = df['close'].astype(int).rolling(60).mean()
-    df['ma120'] = df['close'].astype(int).rolling(120).mean()
+    normal_log.set_loglevel("I")
 
-    df['ema10'] = df['close'].ewm(span=9, adjust=False).mean()
+    df = get_income(ticker, "minute1")
 
     current_stage = ma_stage(df['ma20'].iloc[-1], df['ma60'].iloc[-1], df['ma120'].iloc[-1])
 
     if current_stage == 'stage1':
         stage1(df['close'], df['ma20'], df['ma60'], df['ma120'])
-
-    if current_stage == 'stage2':
+    elif current_stage == 'stage2':
         stage2(df['close'], df['ma20'], df['ma60'], df['ma120'])
-
-    if current_stage == 'stage3':
+    elif current_stage == 'stage3':
         stage3(df['close'], df['ma20'], df['ma60'], df['ma120'])
-
-    if current_stage == 'stage4':
+    elif current_stage == 'stage4':
         stage4(df['close'], df['ma20'], df['ma60'], df['ma120'])
-
-    if current_stage == 'stage5':
+    elif current_stage == 'stage5':
         stage5(df['close'], df['ma20'], df['ma60'], df['ma120'])
-
-    if current_stage == 'stage6':
+    elif current_stage == 'stage6':
         stage6(df['close'], df['ma20'], df['ma60'], df['ma120'])
 
     time.sleep(60)
--- upbit_module.py
+++ upbit_module.py
@@ -1,6 +1,9 @@
-from log_setting import log_info
-
+from normal_log import set_loglevel
+import fluent_log
+import pyupbit
 import logging
+
+
 def get_balance(upbit, ticker):
     balances = upbit.get_balances()  # Assuming upbit.get_balance() returns a single float
     print(balances)
@@ -45,7 +48,7 @@ def ma_stage(short, middle, long):
     if middle >= long >= short:  # 중 장 단
         return "stage3"
 
-    if long >= middle >= short:  # 장 중 단
+    if long >= middle >= short:  # 장  중 단
         return "stage4"
 
     if long >= short >= middle:  # 장 단 중
@@ -56,90 +59,103 @@ def ma_stage(short, middle, long):
 
 
 def stage1(value, short, middle, long):  # 단 중 장
-    log_info.info(f"===== stage1 : {value.iloc[-1]} 안정하게 상승 중 =====")
+    logging.info(f" stage1 : {value.iloc[-1]} 안정하게 상승 중.")
 
-    if short.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 단기 중기 교차   =============")
+    short_gradient = gradient(short, 20)
 
-    if middle.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"============= 중기 장기 교차   =============")
+    middle_gradient = gradient(middle, 60)
 
-    if middle.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"============= 중기 장기 교차   =============")
+    long_gradient = gradient(long, 120)
 
-    pass
+    if short_gradient > 0 and middle_gradient > 0 and long_gradient > 0:
+        logging.info(" 매수에 엣지 발생. ")
+
+    elif short_gradient < 0 and middle_gradient < 0 and long_gradient < 0:
+        logging.info(" 매도에 엣지 발생. ")
+
+    elif short.iloc[-1] == middle.iloc[-1]:
+        logging.info(f" 단기 중기 교차. ")
+
+    elif middle.iloc[-1] == long.iloc[-1]:
+        logging.info(f" 중기 장기 교차.")
+
+    elif short.iloc[-1] == long.iloc[-1]:
+        logging.info(f" 단기 장기 교차. ")
 
 
 def stage2(value, short, middle, long):  # 중 단 장
-    log_info.info(f"===== stage2 : {value.iloc[-1]} 상승 추세의 끝  =====")
+    logging.info(f" stage2 : {value.iloc[-1]} 상승 추세의 끝. ")
 
     if middle.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"============= 중기 단기 교차  =============")
+        logging.info(f" 중기 단기 교차. ")
 
     if short.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"============= 단기 장기 교차  =============")
+        logging.info(f" 단기 장기 교차. ")
 
     if middle.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"=============  즁기 장기 교차  =============")
-
-    pass
+        logging.info(f" 즁기 장기 교차. ")
 
 
 def stage3(value, short, middle, long):  # 중 장 단
-    log_info.info(f"===== stage3 : {value.iloc[-1]} 하락 추세의 시작 =====")
+    logging.info(f" stage3 : {value.iloc[-1]} 하락 추세의 시작. ")
 
     if middle.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"============= 중기 장기 교차   =============")
+        logging.info(f" 중기 장기 교차. ")
 
     if long.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"=============장기 단기 교차  =============")
+        logging.info(f" 장기 단기 교차. ")
 
     if middle.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"============= 중기 단기 교차  =============")
-
-    pass
+        logging.info(f" 중기 단기 교차. ")
 
 
 def stage4(value, short, middle, long):  # 장 중 단
-    log_info.info(f"===== stage4 : {value.iloc[-1]} 안정하게 하락 중 =====")
+    logging.info(f" stage4 : {value.iloc[-1]} 안정하게 하락 중. ")
 
     if long.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 장기 중기 교차   =============")
+        logging.info(f" 장기 중기 교차. ")
 
     if middle.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"============= 중기 단기 교차  =============")
+        logging.info(f" 중기 단기 교차. ")
 
     if long.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"============= 장기 단기 교차   =============")
-
-    pass
+        logging.info(f" 장기 단기 교차. ")
 
 
 def stage5(value, short, middle, long):  # 장 단 중
-    log_info.info(f"===== stage5 : {value.iloc[-1]} 하락 추세의 끝  =====")
+    logging.info(f" stage5 : {value.iloc[-1]} 하락 추세의 끝. ")
 
     if long.iloc[-1] == short.iloc[-1]:
-        log_info.info(f"============= 장기 단기 교차  =============")
+        logging.info(f" 장기 단기 교차. ")
 
     if short.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 단기 중기 교차  =============")
+        logging.info(f" 단기 중기 교차.  ")
 
     if long.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 장기 중기 교차  =============")
+        logging.info(f" 장기 중기 교차.  ")
 
-    pass
 
 #desire-decide-cartel-peru-milk-1545
 def stage6(value, short, middle, long):  # 단 장 중
-    log_info.info(f"===== stage6 : {value.iloc[-1]} 상승 추세의 시작 =====")
+    logging.info(f" stage6 : {value.iloc[-1]} 상승 추세의 시작. ")
 
     if short.iloc[-1] == long.iloc[-1]:
-        log_info.info(f"============= 단기 장기 교차  =============")
+        logging.info(f" 단기 장기 교차.  ")
 
     if long.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 장기 중기 교차   =============")
+        logging.info(f" 장기 중기 교차. ")
 
     if short.iloc[-1] == middle.iloc[-1]:
-        log_info.info(f"============= 단기 중기 교차   =============")
+        logging.info(f" 단기 중기 교차. ")
 
-    pass
+
+def get_income(ticker, interval):
+    df = pyupbit.get_ohlcv(ticker, interval=interval, count=360)
+    df['close'] = df['close'].astype(int)
+    df['ma20'] = df['close'].astype(int).rolling(20).mean()
+    df['ma60'] = df['close'].astype(int).rolling(60).mean()
+    df['ma120'] = df['close'].astype(int).rolling(120).mean()
+
+    df['ema10'] = df['close'].ewm(span=10, adjust=False).mean()
+
+    return df
