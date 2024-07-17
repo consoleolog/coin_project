e1aaca86bbc5f0128496c66657a132bbb3151a63
consolelog <leopportunity5@gmail.com>
2024-07-17 16:39:58 +0900

    [4] 함수 호출 방법 변경

--- main.py
+++ main.py
@@ -13,19 +13,18 @@ while True:
 
     df = get_income(ticker, "minute1")
 
+    functions = {
+        'stage1': stage1,
+        'stage2': stage2,
+        'stage3': stage3,
+        'stage4': stage4,
+        'stage5': stage5,
+        'stage6': stage6
+    }
+
     current_stage = ma_stage(df['ma20'].iloc[-1], df['ma60'].iloc[-1], df['ma120'].iloc[-1])
 
-    if current_stage == 'stage1':
-        stage1(df['close'], df['ma20'], df['ma60'], df['ma120'])
-    elif current_stage == 'stage2':
-        stage2(df['close'], df['ma20'], df['ma60'], df['ma120'])
-    elif current_stage == 'stage3':
-        stage3(df['close'], df['ma20'], df['ma60'], df['ma120'])
-    elif current_stage == 'stage4':
-        stage4(df['close'], df['ma20'], df['ma60'], df['ma120'])
-    elif current_stage == 'stage5':
-        stage5(df['close'], df['ma20'], df['ma60'], df['ma120'])
-    elif current_stage == 'stage6':
-        stage6(df['close'], df['ma20'], df['ma60'], df['ma120'])
+    if current_stage in functions:
+        functions[current_stage](df['close'], df['ma20'], df['ma60'], df['ma120'])
 
     time.sleep(60)
